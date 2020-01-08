4dc3c4fe1d3336be1f207f5b34a23af8ac4cd274
Vincent Carlino <vincentcarlino@Vincents-MacBook-Pro.local>
2020-01-07 20:51:49 -0500

    Build Lab0

---
+++ lab0_setup/Build/CMakeFiles/Lab0.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/vincentcarlino/code/ComputerGraphics/lab0_setup/BasicWidget.cpp" "/Users/vincentcarlino/code/ComputerGraphics/lab0_setup/Build/CMakeFiles/Lab0.dir/BasicWidget.cpp.o"
+  "/Users/vincentcarlino/code/ComputerGraphics/lab0_setup/Lab0.cpp" "/Users/vincentcarlino/code/ComputerGraphics/lab0_setup/Build/CMakeFiles/Lab0.dir/Lab0.cpp.o"
+  "/Users/vincentcarlino/code/ComputerGraphics/lab0_setup/Build/Lab0_autogen/mocs_compilation.cpp" "/Users/vincentcarlino/code/ComputerGraphics/lab0_setup/Build/CMakeFiles/Lab0.dir/Lab0_autogen/mocs_compilation.cpp.o"
+  "/Users/vincentcarlino/code/ComputerGraphics/lab0_setup/main.cpp" "/Users/vincentcarlino/code/ComputerGraphics/lab0_setup/Build/CMakeFiles/Lab0.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "Lab0_autogen/include"
+  "/Users/vincentcarlino/Qt/5.14.0/clang_64/lib/QtWidgets.framework"
+  "/Users/vincentcarlino/Qt/5.14.0/clang_64/lib/QtWidgets.framework/Headers"
+  "/Users/vincentcarlino/Qt/5.14.0/clang_64/lib/QtGui.framework"
+  "/Users/vincentcarlino/Qt/5.14.0/clang_64/lib/QtGui.framework/Headers"
+  "/System/Library/Frameworks/OpenGL.framework/Headers"
+  "/Users/vincentcarlino/Qt/5.14.0/clang_64/lib/QtCore.framework"
+  "/Users/vincentcarlino/Qt/5.14.0/clang_64/lib/QtCore.framework/Headers"
+  "/Users/vincentcarlino/Qt/5.14.0/clang_64/./mkspecs/macx-clang"
+  "/Users/vincentcarlino/Qt/5.14.0/clang_64/lib/QtOpenGL.framework"
+  "/Users/vincentcarlino/Qt/5.14.0/clang_64/lib/QtOpenGL.framework/Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
