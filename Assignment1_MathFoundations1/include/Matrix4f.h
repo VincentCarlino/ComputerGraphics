33c2e95a408fd29f69df34a766270553c595bf32
Vincent Carlino <vjcarlino22@gmail.com>
2020-01-31 17:20:01 -0500

    Finish most testing for Assignment1

--- Assignment1_MathFoundations1/include/Matrix4f.h
+++ Assignment1_MathFoundations1/include/Matrix4f.h
@@ -18,8 +18,8 @@ private:
 public:
     Matrix4f() = default;
 
-    // TODO: Row or column major order you decide! (This is currently column major)
     // Matrix constructor with 9 scalar values.
+    // Values in the constructor represent each row in the matrix
     Matrix4f( float n00, float n01, float n02, float n03,
               float n10, float n11, float n12, float n13,
               float n20, float n21, float n22, float n23,
@@ -32,12 +32,13 @@ public:
     }
 
     // Matrix constructor from four vectors.
+    // Each vector makes up a column in the matrix
     // Note: 'd' will almost always be 0,0,0,1
     Matrix4f(const Vector4f& a, const Vector4f& b, const Vector4f& c, const Vector4f& d){
-      n[0][0] = a.x; n[0][1] = b.x; n[0][2] = c.x; n[0][3] = d.x;
-      n[1][0] = a.y; n[1][1] = b.y; n[1][2] = c.y; n[1][3] = d.y;
-      n[2][0] = a.z; n[2][1] = b.z; n[2][2] = c.z; n[2][3] = d.z;
-      n[3][0] = a.w; n[3][1] = b.w; n[3][2] = c.w; n[3][3] = d.w;
+      n[0][0] = a.x; n[0][1] = a.y; n[0][2] = a.z; n[0][3] = a.w;
+      n[1][0] = b.x; n[1][1] = b.y; n[1][2] = b.z; n[1][3] = b.w;
+      n[2][0] = c.x; n[2][1] = c.y; n[2][2] = c.z; n[2][3] = c.w;
+      n[3][0] = d.x; n[3][1] = d.y; n[3][2] = d.z; n[3][3] = d.w;
     }
 
     // Makes the matrix an identity matrix
@@ -45,10 +46,10 @@ public:
       for(int x = 0; x <= 3; ++x) {
         for(int y = 0; y <= 3; ++y) {
           if(x == y) {
-            n[x][y] = 1;
+            n[x][y] = 1.0f;
           }
           else {
-            n[x][y] = 0;
+            n[x][y] = 0.0f;
           }
         }
       }
@@ -57,74 +58,98 @@ public:
     // Prints the matrix to cout, mainly for debugging
     void printMatrix() {
       std::cout << "Printing matrix: " << std::endl;
-      std::cout << "[ " << n[0][0] << " " << n[0][1] << " " << n[0][2] << " " << n[0][3] << " ]" << std::endl;
-      std::cout << "[ " << n[1][0] << " " << n[1][1] << " " << n[1][2] << " " << n[1][3] << " ]" << std::endl;
-      std::cout << "[ " << n[2][0] << " " << n[2][1] << " " << n[2][2] << " " << n[2][3] << " ]" << std::endl;
-      std::cout << "[ " << n[3][0] << " " << n[3][1] << " " << n[3][2] << " " << n[3][3] << " ]" << std::endl;
+      std::cout << "[ " << n[0][0] << " " << n[1][0] << " " << n[2][0] << " " << n[3][0] << " ]" << std::endl;
+      std::cout << "[ " << n[0][1] << " " << n[1][1] << " " << n[2][1] << " " << n[3][1] << " ]" << std::endl;
+      std::cout << "[ " << n[0][2] << " " << n[1][2] << " " << n[2][2] << " " << n[3][2] << " ]" << std::endl;
+      std::cout << "[ " << n[0][3] << " " << n[1][3] << " " << n[2][3] << " " << n[3][3] << " ]" << std::endl;
     }
 
     // Index operator with two dimensions
-    // Example: M(1,1) returns row 1 and column 1 of matrix M.
+    // Example: M(1,2) returns row 2 and column 1 of matrix M.
     float& operator ()(int i, int j){
       return (n[i][j]);
     }
 
     // Index operator with two dimensions
-    // Example: M(1,1) returns row 1 and column 1 of matrix M.
+    // Example: M(1,2) returns row 2 and column 1 of matrix M.
     const float& operator ()(int i, int j) const{
       return (n[i][j]);
     }
 
-    // Return a single  vector from the matrix (row)
+    // Return a single  vector from the matrix (column)
     Vector4f& operator [](int j){
       return (*reinterpret_cast<Vector4f *>(n[j]));
     }
 
-    // Return a single  vector from the matrix (row)
+    // Return a single  vector from the matrix (column)
     const Vector4f& operator [](int j) const{
       return (*reinterpret_cast<const Vector4f *>(n[j]));
     }
 
     // Make a matrix rotate about various axis
     Matrix4f MakeRotationX(float t){
-        return(Matrix4f(1.0f,0,0,0,
-                        0,cos(t),-(sin(t)),0,
-                        0,sin(t),cos(t),0,
-                        0,0,0,1.0f));
+        return(Matrix4f(1.0f,0,       0,      0,
+                        0,   cos(t),  sin(t), 0,
+                        0,   -(sin(t)),cos(t),0,
+                        0,   0,        0,     1.0f));
     }
     Matrix4f MakeRotationY(float t){
-        return(Matrix4f(cos(t),0,sin(t),0,
-                        0,1.0f,0,0,
-                        -(sin(t)),0,cos(t),0,
-                        0,0,0,1.0f));
+        return(Matrix4f(cos(t),   0,    -(sin(t)), 0,
+                        0,        1.0f, 0,      0,
+                        sin(t),   0,    cos(t), 0,
+                        0,        0,    0,      1.0f));
     }
     Matrix4f MakeRotationZ(float t){
-        return(Matrix4f(cos(t),-(sin(t)),0,0,
-                        sin(t),cos(t),0,0,
-                        0,0,1.0f,0,
-                        0,0,0,1.0f)); 
+        return(Matrix4f(cos(t),   sin(t),   0,    0,
+                        -(sin(t)),cos(t),   0,    0,
+                        0,        0,        1.0f, 0,
+                        0,        0,        0,    1.0f)); 
     }
     Matrix4f MakeScale(float sx,float sy, float sz){
-        return(Matrix4f(sx,0,0,0,
-                        0,sy,0,0,
-                        0,0,sz,0,
-                        0,0,0,1.0f));
+      n[0][0] *= sx;
+      n[0][1] *= sx;
+      n[0][2] *= sx;
+      n[0][3] *= sx;
+
+      n[1][0] *= sy;
+      n[1][1] *= sy;
+      n[1][2] *= sy;
+      n[1][3] *= sy;
+
+      n[2][0] *= sz;
+      n[2][1] *= sz;
+      n[2][2] *= sz;
+      n[2][3] *= sz;
+
     }
 };
 
-Vector4f col(Matrix4f m, int i) {
+Vector4f row(Matrix4f m, int i) {
   Vector4f vec(m(0, i), m(1, i), m(2, i), m(3, i));
   return vec;
 }
 
-// Matrix Multiplication
+// // Matrix Multiplication
+// Matrix4f operator *(const Matrix4f& A, const Matrix4f& B){
+//   Matrix4f mat4;
+//   Vector4f r;
+//   for(int x = 0; x <=3; ++x) {
+//     for(int y = 0; y <=3; ++y) {
+//       r = row(B, y);
+//       mat4[x][y] = Dot(A[y], r);
+//     }
+//   }
+
+//   return mat4;
+// }
+
+// Matrix Multiplication // ALTERNATE VERSION OF MULTIPLICATION BASED ON GLM
 Matrix4f operator *(const Matrix4f& A, const Matrix4f& B){
   Matrix4f mat4;
-  Vector4f column;
+  Vector4f r;
   for(int x = 0; x <=3; ++x) {
     for(int y = 0; y <=3; ++y) {
-      column = col(B, x);
-      mat4[y][x] = Dot(A[y], column);
+      mat4[x][y] = A[x][y] * B[x][y];
     }
   }
 
--- Assignment1_MathFoundations1/src/main.cpp
+++ Assignment1_MathFoundations1/src/main.cpp
@@ -10,6 +10,50 @@
 #include <glm/gtx/transform.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+void printGlmMatrix(glm::mat4 n) {
+      std::cout << "Printing matrix: " << std::endl;
+      std::cout << "[ " << n[0][0] << " " << n[1][0] << " " << n[2][0] << " " << n[3][0] << " ]" << std::endl;
+      std::cout << "[ " << n[0][1] << " " << n[1][1] << " " << n[2][1] << " " << n[3][1] << " ]" << std::endl;
+      std::cout << "[ " << n[0][2] << " " << n[1][2] << " " << n[2][2] << " " << n[3][2] << " ]" << std::endl;
+      std::cout << "[ " << n[0][3] << " " << n[1][3] << " " << n[2][3] << " " << n[3][3] << " ]" << std::endl;
+    }
+
+void printGlmVector(glm::vec4 n){
+    std::cout << "Printing Vector:  " << std::endl;
+    std::cout << "[ " << n[0] <<  " ]" << std::endl;
+    std::cout << "[ " << n[1] <<  " ]" << std::endl;
+    std::cout << "[ " << n[2] <<  " ]" << std::endl;
+    std::cout << "[ " << n[3] <<  " ]" << std::endl;
+
+}
+
+bool areMatricesEqual(glm::mat4 glmMatrix, Matrix4f myMatrix){
+    return glmMatrix[0][0]==myMatrix[0][0] &&
+           glmMatrix[0][1]==myMatrix[0][1] &&
+           glmMatrix[0][2]==myMatrix[0][2] &&
+           glmMatrix[0][3]==myMatrix[0][3] &&
+           glmMatrix[1][0]==myMatrix[1][0] &&
+           glmMatrix[1][1]==myMatrix[1][1] &&
+           glmMatrix[1][2]==myMatrix[1][2] &&
+           glmMatrix[1][3]==myMatrix[1][3] &&
+           glmMatrix[2][0]==myMatrix[2][0] &&
+           glmMatrix[2][1]==myMatrix[2][1] &&
+           glmMatrix[2][2]==myMatrix[2][2] &&
+           glmMatrix[2][3]==myMatrix[2][3] &&
+           glmMatrix[3][0]==myMatrix[3][0] &&
+           glmMatrix[3][1]==myMatrix[3][1] &&
+           glmMatrix[3][2]==myMatrix[3][2] &&
+           glmMatrix[3][3]==myMatrix[3][3];
+}
+
+bool areVectorsEqual(glm::vec4 glmVector, Vector4f myVector){
+    return glmVector[0] == myVector[0] &&
+           glmVector[1] == myVector[1] &&
+           glmVector[2] == myVector[2] &&
+           glmVector[3] == myVector[3];
+}
+
+// ============ MATRIX UNIT TESTS ======================
 // Sample unit test comparing against GLM.
 bool unitTest0(){
 	glm::mat4 glmIdentityMatrix = glm::mat4(1.0f);
@@ -18,60 +62,21 @@ bool unitTest0(){
 		        	     0,0,1.0f,0,
 			             0,0,0,1.0f);
 
-    if(
-        glmIdentityMatrix[0][0]==myIdentity[0][0] &&
-        glmIdentityMatrix[0][1]==myIdentity[0][1] &&
-        glmIdentityMatrix[0][2]==myIdentity[0][2] &&
-        glmIdentityMatrix[0][3]==myIdentity[0][3] &&
-        glmIdentityMatrix[1][0]==myIdentity[1][0] &&
-        glmIdentityMatrix[1][1]==myIdentity[1][1] &&
-        glmIdentityMatrix[1][2]==myIdentity[1][2] &&
-        glmIdentityMatrix[1][3]==myIdentity[1][3] &&
-        glmIdentityMatrix[2][0]==myIdentity[2][0] &&
-        glmIdentityMatrix[2][1]==myIdentity[2][1] &&
-        glmIdentityMatrix[2][2]==myIdentity[2][2] &&
-        glmIdentityMatrix[2][3]==myIdentity[2][3] &&
-        glmIdentityMatrix[3][0]==myIdentity[3][0] &&
-        glmIdentityMatrix[3][1]==myIdentity[3][1] &&
-        glmIdentityMatrix[3][2]==myIdentity[3][2] &&
-        glmIdentityMatrix[3][3]==myIdentity[3][3]){
-            return true;
-    }
-	
-    return false;	
+    return areMatricesEqual(glmIdentityMatrix, myIdentity);
 }
 
+// Tests Matrix4f.identity() against glm
 bool unitTest1(){
 	glm::mat4 glmIdentityMatrix = glm::mat4(1.0f);
-	Matrix4f myIdentity( 1.0f,0,0,0,
-        			     0,1.0f,0,0,
-		        	     0,0,1.0f,0,
-			             0,0,0,1.0f);
-
-    if(
-        glmIdentityMatrix[0][0]==myIdentity(0,0) &&
-        glmIdentityMatrix[0][1]==myIdentity(0,1) &&
-        glmIdentityMatrix[0][2]==myIdentity(0,2) &&
-        glmIdentityMatrix[0][3]==myIdentity(0,3) &&
-        glmIdentityMatrix[1][0]==myIdentity(1,0) &&
-        glmIdentityMatrix[1][1]==myIdentity(1,1) &&
-        glmIdentityMatrix[1][2]==myIdentity(1,2) &&
-        glmIdentityMatrix[1][3]==myIdentity(1,3) &&
-        glmIdentityMatrix[2][0]==myIdentity(2,0) &&
-        glmIdentityMatrix[2][1]==myIdentity(2,1) &&
-        glmIdentityMatrix[2][2]==myIdentity(2,2) &&
-        glmIdentityMatrix[2][3]==myIdentity(2,3) &&
-        glmIdentityMatrix[3][0]==myIdentity(3,0) &&
-        glmIdentityMatrix[3][1]==myIdentity(3,1) &&
-        glmIdentityMatrix[3][2]==myIdentity(3,2) &&
-        glmIdentityMatrix[3][3]==myIdentity(3,3)){
-            return true;
-    }
-	
-    return false;	
+	Matrix4f myIdentity( 0,0,0,0,
+        			     0,0,0,0,
+		        	     0,0,0,0,
+			             0,0,0,0);
+    myIdentity.identity();
+    return areMatricesEqual(glmIdentityMatrix, myIdentity);
 }
 
-// Sample unit test comparing against GLM.
+// Tests constructor using 4 vectors
 bool unitTest2(){
 	glm::mat4 glmIdentityMatrix = glm::mat4(1.0f);
 	Vector4f a(1.0f ,0.0f,  0.0f,   0.0f);
@@ -79,62 +84,25 @@ bool unitTest2(){
 	Vector4f c(0.0f ,0.0f,  1.0f,   0.0f);
 	Vector4f d(0.0f ,0.0f,  0.0f,   1.0f);
 	Matrix4f myIdentity(a,b,c,d);
+    return areMatricesEqual(glmIdentityMatrix, myIdentity);
 
-    if(
-        glmIdentityMatrix[0][0]==myIdentity[0][0] &&
-        glmIdentityMatrix[0][1]==myIdentity[0][1] &&
-        glmIdentityMatrix[0][2]==myIdentity[0][2] &&
-        glmIdentityMatrix[0][3]==myIdentity[0][3] &&
-        glmIdentityMatrix[1][0]==myIdentity[1][0] &&
-        glmIdentityMatrix[1][1]==myIdentity[1][1] &&
-        glmIdentityMatrix[1][2]==myIdentity[1][2] &&
-        glmIdentityMatrix[1][3]==myIdentity[1][3] &&
-        glmIdentityMatrix[2][0]==myIdentity[2][0] &&
-        glmIdentityMatrix[2][1]==myIdentity[2][1] &&
-        glmIdentityMatrix[2][2]==myIdentity[2][2] &&
-        glmIdentityMatrix[2][3]==myIdentity[2][3] &&
-        glmIdentityMatrix[3][0]==myIdentity[3][0] &&
-        glmIdentityMatrix[3][1]==myIdentity[3][1] &&
-        glmIdentityMatrix[3][2]==myIdentity[3][2] &&
-        glmIdentityMatrix[3][3]==myIdentity[3][3]){
-            return true;
-    }
-	
-    return false;	
 }
 
 // Sample unit test comparing against GLM.
-// TODO: Test against glm::scale
 bool unitTest3(){
-	glm::mat4 glmScale = glm::mat4(2.0f);
-	Vector4f a(1.0f,0,0,0);
-	Vector4f b(0.0f,1.0f,0,0);
-	Vector4f c(0,0,1.0f,0);
-	Vector4f d(0,0,0,1.0f);
-	Matrix4f myScaled(a,b,c,d);
+	glm::mat4 glmScale = glm::mat4(1.0f, 1.0f, 2.0f, 1.0f,
+                                    0.0f, 2.0f, 0.0f, 3.0f,
+                                    1.0f, 2.0f, 3.0f, 0.0f,
+                                    0.0f, 0.0f, 2.0f, 1.0f);
+    glmScale = glm::scale(glmScale, glm::vec3(2.0f, 2.0f, 2.0f));
+	Matrix4f myScaled(1.0f, 1.0f, 2.0f, 1.0f,
+                      0.0f, 2.0f, 0.0f, 3.0f,
+                      1.0f, 2.0f, 3.0f, 0.0f,
+                      0.0f, 0.0f, 2.0f, 1.0f);
+
     myScaled.MakeScale(2.0f,2.0f,2.0f);
 
-    if(
-        glmScale[0][0]==myScaled[0][0] &&
-        glmScale[0][1]==myScaled[0][1] &&
-        glmScale[0][2]==myScaled[0][2] &&
-        glmScale[0][3]==myScaled[0][3] &&
-        glmScale[1][0]==myScaled[1][0] &&
-        glmScale[1][1]==myScaled[1][1] &&
-        glmScale[1][2]==myScaled[1][2] &&
-        glmScale[1][3]==myScaled[1][3] &&
-        glmScale[2][0]==myScaled[2][0] &&
-        glmScale[2][1]==myScaled[2][1] &&
-        glmScale[2][2]==myScaled[2][2] &&
-        glmScale[2][3]==myScaled[2][3] &&
-        glmScale[3][0]==myScaled[3][0] &&
-        glmScale[3][1]==myScaled[3][1] &&
-        glmScale[3][2]==myScaled[3][2] &&
-        glmScale[3][3]==myScaled[3][3]){
-            return true;
-    }
-	
-    return false;	
+    return areMatricesEqual(glmScale, myScaled);
 }
 
 // Sample unit test comparing against GLM.
@@ -152,12 +120,8 @@ bool unitTest4(){
     myMatrix[1][3] = 72.0f;
     myMatrix[2][3] = 2.1f;
 
-    if( glmTest[1][3]==myMatrix[1][3] &&
-        glmTest[2][3]==myMatrix[2][3] ){
-            return true;
-    }
-	
-    return false;	
+    return glmTest[1][3]==myMatrix[1][3] &&
+           glmTest[2][3]==myMatrix[2][3];
 }
 
 // Sample unit test testing your library
@@ -172,9 +136,280 @@ bool unitTest5(){
     return false;
 }
 
+// Tests glm vec4 against our library
+bool unitTest6(){
+    Vector4f a(1, 2, 3, 4);
+    glm::vec4 b(1, 2, 3,4);
+
+    return areVectorsEqual(b, a);
+
+}
+
+// Tests the [] operator against a glm vector
+bool unitTest7(){
+    glm::vec4 glmVec(0.0f, 0.0f, 1.0f, 0.0f);
+
+    Vector4f a(1.0f ,0.0f,  0.0f,   0.0f);
+	Vector4f b(0.0f ,1.0f,  0.0f,   0.0f);
+	Vector4f c(0.0f ,0.0f,  1.0f,   0.0f);
+	Vector4f d(0.0f ,0.0f,  0.0f,   1.0f);
+	Matrix4f myIdentity(a,b,c,d);
+
+    Vector4f myVector = myIdentity[2];
+    return areVectorsEqual(glmVec, myVector);
+}
+
+// Tests the rotation matrices X, Y, and Z against glm
+bool unitTest8(){
+    glm::mat4 glmMatrixX(1.0f);
+    glm::mat4 glmMatrixY(1.0f);
+    glm::mat4 glmMatrixZ(1.0f);
+    glmMatrixX = glm::rotate(glmMatrixX, glm::radians(180.0f), glm::vec3(1, 0, 0));
+    glmMatrixY = glm::rotate(glmMatrixY, glm::radians(40.0f), glm::vec3(0, 1, 0));
+    glmMatrixZ = glm::rotate(glmMatrixZ, glm::radians(10.0f), glm::vec3(0, 0, 1));
+
+    Vector4f a(1.0f ,0.0f,  0.0f,   0.0f);
+	Vector4f b(0.0f ,1.0f,  0.0f,   0.0f);
+	Vector4f c(0.0f ,0.0f,  1.0f,   0.0f);
+	Vector4f d(0.0f ,0.0f,  0.0f,   1.0f);
+	Matrix4f myMatrixX(a,b,c,d);
+    Matrix4f myMatrixY(a,b,c,d);
+    Matrix4f myMatrixZ(a,b,c,d);
+
+    myMatrixX = myMatrixX.MakeRotationX(glm::radians(180.0f));
+    myMatrixY = myMatrixY.MakeRotationY(glm::radians(40.0f));
+    myMatrixZ = myMatrixZ.MakeRotationZ(glm::radians(10.0f));
+
+    return areMatricesEqual(glmMatrixX, myMatrixX) && areMatricesEqual(glmMatrixY, myMatrixY) && areMatricesEqual(glmMatrixZ, myMatrixZ);
+} 
+
+// TODO: Test a matrix multiplied by another matrix
+bool unitTest9(){
+    Matrix4f myA(1.0f, 0.0f, 0.0f, 0.0f, 
+                0.0f, 1.0f, 0.0f, 0.0f, 
+                0.0f, 0.0f, 1.0f, 0.0f, 
+                0.0f, 0.0f, 0.0f, 1.0f);
+
+    Matrix4f myB(3.0f, 0.0f, 0.0f, 0.0f, 
+                0.0f, 3.0f, 0.0f, 0.0f, 
+                0.0f, 0.0f, 3.0f, 0.0f, 
+                0.0f, 0.0f, 0.0f, 3.0f);
+
+    glm::mat4 glmA(1.0f);
+    glm::mat4 glmB(3.0f);
+
+    myA = myA * myB;
+    glmA = glm::matrixCompMult(glmA, glmB);
+
+    return areMatricesEqual(glmA, myA);
+}
+
+// Test a matrix multiplied by a vector
+bool unitTest10(){
+
+    glm::mat4 glmMatrix(1.0f);
+    glm::vec4 vec(4.0, 1.0f, 1.0f, 1.0f);
+    Vector4f a(4.0f , 1.0f, 1.0f, 1.0f);
+
+	Matrix4f myMatrix(1.0f, 0.0f, 0.0f, 0.0f, 
+                      0.0f, 1.0f, 0.0f, 0.0f, 
+                      0.0f, 0.0f, 1.0f, 0.0f, 
+                      0.0f, 0.0f, 0.0f, 1.0f);
+
+    Vector4f myVector;
+    glm::vec4 glmVector;
+
+    myVector = myMatrix * a;
+    glmVector = glmMatrix * vec;
+
+    return areVectorsEqual(glmVector, myVector);
+
+}
+
+// ============ VECTOR UNIT TESTS ======================
+
+// Test our constructor
+bool unitTest11(){
+    Vector4f myVector(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmVector(1.0f, 2.0f, 3.0f, 4.0f);
+
+    return areVectorsEqual(glmVector, myVector);
+
+}
+
+// Test scalar vector *=
+bool unitTest12(){
+    Vector4f myVector(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmVector(1.0f, 2.0f, 3.0f, 4.0f);
+
+    myVector *= 5.0f;
+    glmVector *= 5.0f;
+
+    return areVectorsEqual(glmVector, myVector);
+}
+
+// Test scalar vector /=
+bool unitTest13(){
+    Vector4f myVector(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmVector(1.0f, 2.0f, 3.0f, 4.0f);
+
+    myVector /= 5.0f;
+    glmVector /= 5.0f;
+
+    return areVectorsEqual(glmVector, myVector);
+}
+
+// Test vector +=
+bool unitTest14(){
+    Vector4f myA(1.0f, 2.0f, 3.0f, 4.0f);
+    Vector4f myB(2.0f, 3.0f, 4.0f, 1.0f);
+    glm::vec4 glmA(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmB(2.0f, 3.0f, 4.0f, 1.0f);
+
+    myA += myB;
+    glmA += glmB;
+
+    return areVectorsEqual(glmA, myA);
+}
+
+// Test vector -=
+bool unitTest15(){
+    Vector4f myA(1.0f, 2.0f, 3.0f, 4.0f);
+    Vector4f myB(2.0f, 3.0f, 4.0f, 1.0f);
+    glm::vec4 glmA(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmB(2.0f, 3.0f, 4.0f, 1.0f);
+
+    myA -= myB;
+    glmA -= glmB;
+
+    return areVectorsEqual(glmA, myA);
+}
+
+// Test vector scalar multiplication
+bool unitTest16(){
+    Vector4f myA(1.0f, 2.0f, 3.0f, 4.0f);
+    Vector4f myB;
+    glm::vec4 glmA(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmB;
+
+    myB = myA * 5.0f;
+    glmB = glmA * 5.0f;
+
+    return areVectorsEqual(glmB, myB);
+}
+
+// Test vector scalar division
+bool unitTest17(){
+    Vector4f myA(1.0f, 2.0f, 3.0f, 4.0f);
+    Vector4f myB;
+    glm::vec4 glmA(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmB;
+
+    myB = myA / 5.0f;
+    glmB = glmA / 5.0f;
+
+    return areVectorsEqual(glmB, myB);
+}
+
+// Test vector negation
+bool unitTest18(){
+    Vector4f myA(1.0f, 2.0f, 3.0f, 4.0f);
+    Vector4f myB;
+    glm::vec4 glmA(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmB;
+
+    myB = -myA;
+    glmB = -glmA;
+
+    return areVectorsEqual(glmB, myB);
+}
+
+// Test vector addition
+bool unitTest19(){
+    Vector4f myA(1.0f, 2.0f, 3.0f, 4.0f);
+    Vector4f myB(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmA(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmB(1.0f, 2.0f, 3.0f, 4.0f);
+
+    Vector4f myC;
+    glm::vec4 glmC;
+
+    myC = myA + myB;
+    glmC = glmA + glmB;
+
+    return areVectorsEqual(glmC, myC);
+}
+
+// Test vector subtraction
+bool unitTest20(){
+    Vector4f myA(1.0f, 2.0f, 3.0f, 4.0f);
+    Vector4f myB(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmA(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmB(1.0f, 2.0f, 3.0f, 4.0f);
+
+    Vector4f myC;
+    glm::vec4 glmC;
+
+    myC = myA - myB;
+    glmC = glmA - glmB;
+
+    return areVectorsEqual(glmC, myC);
+}
+
+// Test vector magnitude
+bool unitTest21(){
+    Vector4f myA(1.0f, 2.0f, 3.0f, 4.0f);
+    glm::vec4 glmA(1.0f, 2.0f, 3.0f, 4.0f);
+
+    return Magnitude(myA) == glm::length(glmA);
+}
+
+// Test normalizing a vector
+bool unitTest22(){
+    Vector4f myA(1.0f, 2.0f, 3.0f, 4.0f);
+    myA = Normalize(myA);
+    float mag = Magnitude(myA);
+
+    return (abs(mag - 1.0f)) < 0.0000001f; // The magnitude doesn't evaluate exactly to 1, so we check the delta is in an acceptable range
+}
+
+// Test dot product of vector a and vector b
+bool unitTest23(){
+    Vector4f myA(1.0f, 2.0f, 3.0f, 0.0f);
+    Vector4f myB(2.0f, 4.0f, 3.0f, 0.0f);
+
+    glm::vec4 glmA(1.0f, 2.0f, 3.0f, 0.0f);
+    glm::vec4 glmB(2.0f, 4.0f, 3.0f, 0.0f);
+
+    float myDot = Dot(myA, myB);
+    float glmDot = glm::dot(glmA, glmB);
+
+    return glmDot == myDot;
+
+}
+
+bool unitTest24(){
+    Vector4f myA(1.0f, 2.0f, 3.0f, 0.0f);
+    Vector4f myB(2.0f, 4.0f, 3.0f, 0.0f);
+
+    myA = CrossProduct(myA, myB);
+
+    glm::vec3 glmA(1.0f, 2.0f, 3.0f);
+    glm::vec3 glmB(2.0f, 4.0f, 3.0f);
+
+    glmA = glm::cross(glmA, glmB);
+
+    return glmA[0] == myA[0] &&
+            glmA[1] == myA[1] &&
+            glmA[2] == myA[2];
+
+}
+
 int main(){
+    // TODO:
     // Keep track of the tests passed
     unsigned int testsPassed = 0;
+    unsigned int testsRun = 0;
 
     // Run 'unit tests'
     std::cout << "Passed 0: " << unitTest0() << " \n";
@@ -183,24 +418,28 @@ int main(){
     std::cout << "Passed 3: " << unitTest3() << " \n";
     std::cout << "Passed 4: " << unitTest4() << " \n";
     std::cout << "Passed 5: " << unitTest5() << " \n";
+    std::cout << "Passed 6: " << unitTest6() << " \n";
+    std::cout << "Passed 7: " << unitTest7() << " \n";
+    std::cout << "Passed 8: " << unitTest8() << " \n";
+    std::cout << "Passed 9: " << unitTest9() << " \n";
+    std::cout << "Passed 10: " << unitTest10() << " \n";
+    std::cout << "Passed 11: " << unitTest11() << " \n";
+    std::cout << "Passed 12: " << unitTest12() << " \n";
+    std::cout << "Passed 13: " << unitTest13() << " \n";
+    std::cout << "Passed 14: " << unitTest14() << " \n";
+    std::cout << "Passed 15: " << unitTest15() << " \n";
+    std::cout << "Passed 16: " << unitTest16() << " \n";
+    std::cout << "Passed 17: " << unitTest17() << " \n";
+    std::cout << "Passed 18: " << unitTest18() << " \n";
+    std::cout << "Passed 19: " << unitTest19() << " \n";
+    std::cout << "Passed 20: " << unitTest20() << " \n";
+    std::cout << "Passed 21: " << unitTest21() << " \n";
+    std::cout << "Passed 22: " << unitTest22() << " \n";
+    std::cout << "Passed 23: " << unitTest23() << " \n";
+    std::cout << "Passed 24: " << unitTest24() << " \n";
 
-    Vector4f myVector(1, 2, 3, 4);
-    myVector.printVector();
 
-    Matrix4f myMatrix(0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3);
 
-    Vector4f b(0, 1, 1, 0);
-    Vector4f a(1, 2, 3, 0);
-    Vector4f projection = Project(a, b);
-
-    projection.printVector();
-    // Vector4f row0 = myMatrix[2];
-    Vector4f col0 = col(myMatrix, 0);
-    // myMatrix.printMatrix();
-    // row0.printVector();
-    col0.printVector();
-
-    // std::cout << Magnitude(myVector) << std::endl;
 
     return 0;
 }
